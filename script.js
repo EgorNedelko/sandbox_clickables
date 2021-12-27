24d7552b3b9365149c090616b9dec622b5845fee
Egor Nedelko <gojiramedia@gmail.com>
2021-12-27 11:48:42 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const clickables = [
+   document.getElementById('clickableHeading1'),
+   document.getElementById('clickableHeading2'),
+   document.getElementById('clickableHeading3'),
+   document.getElementById('clickableHeading4'),
+   document.getElementById('clickableHeading5'),
+   document.getElementById('clickableHeading6'),
+   document.getElementById('clickableParagraph'),
+   document.getElementById('clickableLink'),
+   document.getElementById('clickableListItem'),
+   document.getElementById('clickableDIV'),
+   document.getElementById('clickableSPAN'),
+   document.getElementById('clickableIMG')
+]
+
+clickables.forEach(el => {
+   el.addEventListener('click', () => {
+      el.classList.add('pressed')
+      setTimeout(()=>{
+         el.classList.remove('pressed')
+      }, 115)
+   })
+})
